c1475efbd01792a61e92d3f9160f5de0f9a42338
ameurbesbes <ameurbesbes@outlook.com>
2022-04-22 07:39:28 +0200

    Update

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Rotator : MonoBehaviour
 {
+    public float degreesPerSecond = 100.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(45 * Time.deltaTime, 0, 0);
+        transform.Rotate(degreesPerSecond * Time.deltaTime, 0, 0);
     }
 }
